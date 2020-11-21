03da02ca13886189615e7be53287c0dc658c7a82
Olusola Afikode <afikodeolaitan@gmail.com>
2020-11-21 11:54:56 +0100

    delete avatar from todo.js

--- src/Todo.js
+++ src/Todo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Todo.css';
-import { List, ListItem, ListItemText, ListItemAvatar, Avatar } from '@material-ui/core';
+import { List, ListItem, ListItemText, ListItemAvatar } from '@material-ui/core';
 
 function Todo(props) {
 	return (
